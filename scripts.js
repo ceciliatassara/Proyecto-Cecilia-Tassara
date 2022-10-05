2215dadc39b66b6362d2d0ca896c7a93bb143d8c
ceciliatassara <ceciliatassara1005@hotmail.com>
2022-10-04 22:40:14 -0300

    js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,25 @@
 
-const nombre = prompt("Hola ingresa tu nombre")
-const texto = "Bienvenido" + nombre
-alert(texto)
+let nombreUsuario = prompt("Ingresar nombre de usuario");
+let ingreseUnNumero = parseInt((prompt "ingrese el numero 40"))
+let numero = 40
+if (numero == 40) {
+    alert("la suma es igual a 40")
+}
+
+let variable1 = 20;
+let variable2 = 20;
+let suma = variable1 + variable2;
+
+if(suma == 40) {
+    console.log("La suma es 40");
+}
+if(suma == 50) {
+    console.log("La suma es 50");
+}
+if(suma == 40) {
+    console.log("La suma es 40");
+} else {
+    console.log("La suma no es 40");
+}
+
+
